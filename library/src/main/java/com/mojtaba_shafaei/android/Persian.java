1677cf15c57b7422cf9ee020322bbc54bef00ddd
mojtaba <shafaei.saeid@gmail.com>
2017-08-28 16:40:45 +0430

    Initial commit

---
+++ library/src/main/java/com/mojtaba_shafaei/android/Persian.java
@@ -0,0 +1,59 @@
+package com.mojtaba_shafaei.android;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+
+class Persian {
+
+    private static Locale loc;
+    private static NumberFormat nf;
+
+    public static NumberFormat getNumberFormat() {
+        if (loc == null) loc = new Locale("fa", "FA");
+        if (nf == null) nf = NumberFormat.getNumberInstance(loc);
+        return nf;
+    }
+
+    public static String formatInteger(String input) {
+        return formatInteger(Integer.parseInt(input));
+    }
+
+    public static String formatInteger(int input) {
+        return getNumberFormat().format(input);
+    }
+
+    public static String formatDobule(String input) {
+        return formatDobule(Double.parseDouble(input));
+    }
+
+    public static String formatDobule(double input) {
+        return getNumberFormat().format(input);
+    }
+
+    public static String format(String input) {
+        if (null == input || input.isEmpty()) {
+            return "";
+        }
+        String[][] replacements = {
+                {"ا", "ا"}, {"أ", "ا"}, {"آ", "آ"}, {"ب", "ب"}, {"پ", "پ"}, {"ت", "ت"}, {"ث", "ث"}, {"ج", "ج"},
+                {"چ", "چ"}, {"ح", "ح"}, {"خ", "خ"}, {"د", "د"}, {"ذ", "ذ"}, {"ر", "ر"}, {"ز", "ز"}, {"ژ", "ژ"},
+                {"س", "س"}, {"ش", "ش"}, {"ص", "ص"}, {"ض", "ض"}, {"ط", "ط"}, {"ظ", "ظ"}, {"ع", "ع"}, {"غ", "غ"},
+                {"ف", "ف"}, {"ق", "ق"}, {"ک", "ک"}, {"ك", "ک"}, {"گ", "گ"}, {"ل", "ل"}, {"م", "م"}, {"ن", "ن"},
+                {"و", "و"}, {"ؤ", "و"}, {"ه", "ه"}, {"ة", "ه"}, {"ئ", "ئ"}, {"ى", "ی"}, {"ي", "ی"}, {"ی", "ی"},
+                {"0", "۰"}, {"1", "۱"}, {"2", "۲"}, {"3", "۳"}, {"4", "۴"}, {"5", "۵"}, {"6", "۶"}, {"7", "۷"},
+                {"8", "۸"}, {"9", "۹"}, {"٠", "۰"}, {"١", "۱"}, {"٢", "۲"}, {"٣", "۳"}, {"٤", "۴"}, {"٥", "۵"},
+                {"٦", "۶"}, {"٧", "۷"}, {"٨", "۸"}, {"٩", "۹"}
+        };
+
+        for (String[] replacement : replacements) {
+            input = input.replace(replacement[0], replacement[1]);
+        }
+
+        return input;
+    }
+
+    public static String rtl(String input) {
+        return "\u200F" + input;
+    }
+
+}
---
+++ library/src/main/java/com/mojtaba_shafaei/android/TextView.java
@@ -0,0 +1,120 @@
+package com.mojtaba_shafaei.android;
+
+import android.animation.Animator;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.support.v7.widget.AppCompatTextView;
+import android.util.AttributeSet;
+
+public class TextView extends AppCompatTextView {
+    private volatile boolean showNumberAsPersian = false;
+
+    public TextView(Context context) {
+        super(context);
+        init(context);
+    }
+
+    public TextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+        readAttrs(context, attrs);
+    }
+
+    public TextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context);
+        readAttrs(context, attrs);
+    }
+
+    private void init(Context context) {
+        if (!isInEditMode()) {
+            //setLineSpacing(1.5f, 1f);
+            setTextIsSelectable(false);
+        }
+    }
+
+    @Override
+    public void setText(CharSequence text, BufferType type) {
+        if (showNumberAsPersian) {
+            text = Persian.format(text.toString());
+        }
+        super.setText(text, type);
+    }
+
+    public void setTextWithAnimation(final CharSequence text, ObjectAnimator... animators) {
+        AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.setDuration(500);
+
+        if (animators == null || animators.length == 0) {
+            animators = new ObjectAnimator[2];
+            animators[0] = ObjectAnimator.ofFloat(this, "alpha", 1f, 0f);
+            animators[0].addListener(new Animator.AnimatorListener() {
+                @Override
+                public void onAnimationStart(Animator animator) {
+
+                }
+
+                @Override
+                public void onAnimationEnd(Animator animator) {
+                    setText("");
+                }
+
+                @Override
+                public void onAnimationCancel(Animator animator) {
+
+                }
+
+                @Override
+                public void onAnimationRepeat(Animator animator) {
+
+                }
+            });
+            animators[1] = ObjectAnimator.ofFloat(this, "alpha", 0f, 1f);
+            animators[1].addListener(new Animator.AnimatorListener() {
+                @Override
+                public void onAnimationStart(Animator animator) {
+                    setText(text);
+                }
+
+                @Override
+                public void onAnimationEnd(Animator animator) {
+                    setAlpha(1f);
+                }
+
+                @Override
+                public void onAnimationCancel(Animator animator) {
+
+                }
+
+                @Override
+                public void onAnimationRepeat(Animator animator) {
+
+                }
+            });
+        }
+        animatorSet.playSequentially(animators);
+        animatorSet.start();
+    }
+
+    private void readAttrs(Context context, AttributeSet attrs) {
+        TypedArray a = context.getTheme().obtainStyledAttributes(
+                attrs,
+                R.styleable.TextView,
+                0, 0);
+
+        try {
+            Integer typeFace = a.getInteger(R.styleable.TextView_fontType, PersianFont.IRANSANS_NORMAL);
+            setTypeface(PersianFont.get(context, typeFace));
+
+            showNumberAsPersian = a.getBoolean(R.styleable.TextView_showDigitAsPersian, false);
+            if (showNumberAsPersian) {
+                setText(Persian.format(getText().toString()));
+            }
+        } finally {
+            a.recycle();
+        }
+    }
+
+}
