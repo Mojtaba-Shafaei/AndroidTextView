6dd62a3a4318504ea1fc9f2930bc706b3f798077
mojtaba <shafaei.saeid@gmail.com>
2018-12-01 11:21:46 +0330

    migrate back to app-comapt upgrade to android sdk 28

--- app/src/main/java/com/mojtaba_shafaei/android/androidTextViewExample/MainActivity.java
+++ app/src/main/java/com/mojtaba_shafaei/android/androidTextViewExample/MainActivity.java
@@ -1,19 +1,19 @@
 package com.mojtaba_shafaei.android.androidTextViewExample;
 
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
 import com.mojtaba_shafaei.androidTextView.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-    TextView textView1 = findViewById(R.id.tv1);
-    textView1.setText("Hello World!");
-    textView1.setCompoundDrawablesWithIntrinsicBounds(
-        ContextCompat.getDrawable(this, R.drawable.ic_3d_rotation_black_24dp), null, null, null);
-  }
+@Override
+protected void onCreate(Bundle savedInstanceState){
+  super.onCreate(savedInstanceState);
+  setContentView(R.layout.activity_main);
+  TextView textView1 = findViewById(R.id.tv1);
+  textView1.setText("Hello World!");
+  textView1.setCompoundDrawablesWithIntrinsicBounds(
+      ContextCompat.getDrawable(this, R.drawable.ic_3d_rotation_black_24dp), null, null, null);
+}
 }
